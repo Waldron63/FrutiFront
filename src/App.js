01c2f8c12674a5cfd0a31ddf311e1791ec5f088d
hakki17 <mariapaulasm2018@gmail.com>
2025-03-30 12:26:51 -0500

    plantillas vista labs y form crear lab hecho, faltan conexiones entre ambas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-function App(){
-    
+// src/App.js
+import React from "react";
+import Laboratorios from "./pages/Laboratorios";
+import CrearLaboratorio from "./pages/CrearLaboratorio";
+import AdminLaboratorios from "./pages/AdminLaboratorios";
+
+function App() {
+  /*return (
+    <div>
+      <Laboratorios />
+    </div>
+  );*/
+  return <CrearLaboratorio />;
+  //return <AdminLaboratorios />;
+  //return <Laboratorios />;
 }
+
+export default App;
